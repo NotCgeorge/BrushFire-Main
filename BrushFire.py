f2f7de68bd0e8312d9a0e33b11fff1f6672b420b
NotCgeorge <135914790+NotCgeorge@users.noreply.github.com>
2023-07-31 17:47:15 -0700

    Update Start BrushFire.py 0.2

--- BrushFire.py
+++ BrushFire.py
@@ -1,17 +1,6 @@
 import pygame
-pygame.init()
 import random
 import time
-
-background_color = (234, 212, 252)
-w = pygame.display.set_mode((300, 300))
-pygame.display.set_caption("BrushFire")
-on = True
-while on:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            on = False
-
 smart = 0
 strength = 0
 speed = 0
@@ -55,6 +44,49 @@ while (int(start1)) <= 5:
         start1 = 100
         start1 = input("1, 2, 3, 4 : ")
 
+print("Alright " + (str(mycode)) + " what is your personality? ")
+print("Logician. (+3 smart)" )
+print("Commander. (+2 strength +1 talk)" )
+print("Exective. (+3 talk)" )
+print("Entrepreneur. (+3 speed)" )
+print("Advocate. (+1 magic +2 smart)" )
+
+start2 = 100
+start2 = input("1, 2, 3, 4, 5 : ")
+while (int(start2)) <= 6:
+    if (int(start2)) == 1:
+        smart += 3
+        print("smart " + (str(smart)))
+        time.sleep(2.5)
+        break
+    elif (int(start2)) == 2:
+        strength += 2
+        talk += 1
+        print("strength " + (str(strength)))
+        print("talk " + (str(talk)))
+        time.sleep(2.5)
+        break
+    elif (int(start2)) == 3:
+        talk += 3
+        print("talk " + (str(talk)))
+        time.sleep(2.5)
+        break
+    elif (int(start2)) == 4:
+        speed += 3
+        print("speed " + (str(speed)))
+        time.sleep(2.5)
+        break
+    elif (int(start2)) == 5:
+        magic += 1
+        smart += 2
+        print("magic " + (str(magic)))
+        print("smart " + (str(smart)))
+        time.sleep(2.5)
+        break
+    elif (int(start2)) <= 6:
+        start2 = 100
+        start2 = input("1, 2, 3, 4, 5 : ")
+
 print("Ok " + (str(mycode)) + " how did you work? ")
 print("As a space juncker. (+2 strength +1 speed)" )
 print("As a trooper. (+5 strength -2 talk)" )
@@ -62,24 +94,24 @@ print("Working as a top administrator of a planet. (+5 talk +2 smart -2 strength
 print("As a logistics wizard. (+4 speed +1 talk) ")
 print("Doing jobs for the local strange man. (+1 magic +2 talk +1 speed)" )
 
-start2 = 100
-start2 = input("1, 2, 3, 4, 5 : ")
-while (int(start2)) <= 6:
-    if (int(start2)) == 1:
+start3 = 100
+start3 = input("1, 2, 3, 4, 5 : ")
+while (int(start3)) <= 6:
+    if (int(start3)) == 1:
         strength += 2
         speed += 1
         print("strength " + (str(strength)))
         print("speed " + (str(speed)))
         time.sleep(2.5)
         break
-    elif (int(start2)) == 2:
+    elif (int(start3)) == 2:
         strength += 5
         talk -= 2
         print("strength " + (str(strength)))
         print("talk " + (str(talk)))
         time.sleep(2.5)
         break
-    elif (int(start2)) == 3:
+    elif (int(start3)) == 3:
         talk += 5
         smart += 2
         strength -= 2
@@ -88,14 +120,14 @@ while (int(start2)) <= 6:
         print("strength " + (str(strength)))
         time.sleep(2.5)
         break
-    elif (int(start2)) == 4:
+    elif (int(start3)) == 4:
         speed += 4
         talk += 1
         print("speed " + (str(speed)))
         print("talk " + (str(talk)))
         time.sleep(2.5)
         break
-    elif (int(start2)) == 5:
+    elif (int(start3)) == 5:
         magic += 1
         talk += 2
         speed += 1
@@ -104,11 +136,7 @@ while (int(start2)) <= 6:
         print("speed " + (str(speed)))
         time.sleep(2.5)
         break
-    elif (int(start2)) <= 6:
-        start2 = 100
-        start2 = input("1, 2, 3, 4, 5 : ")
-pygame.display.flip()
-
-
-
+    elif (int(start3)) <= 6:
+        start3 = 100
+        start3 = input("1, 2, 3, 4, 5 : ")
 
